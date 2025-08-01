a03b52cfe7b0dbbfd87549db697b97aa27517b5e
Vilander Costa <vilander.costa@gmail.com>
2025-08-01 14:08:56 -0300

    style: alterado background de acordo com tipo

--- src/App.jsx
+++ src/App.jsx
@@ -3,84 +3,23 @@ import Card from './Card';
 import Audio from './Audio';
 import Logo from './Logo';
 import Video from './Video';
+import personagens from './pokedex.json';
 
 function App() {
-  const item1 = {
-    id: '#1',
-    nome: 'Bulbassauro',
-    tipo: 'Planta/Venenoso',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
-  }
-  const item2 = {
-    id: '#2',
-    nome: 'Ivysaur',
-    tipo: 'Planta/Venenoso',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png'
-  }
-  const item3 = {
-    id: '#3',
-    nome: 'Venusaur',
-    tipo: 'Planta/Venenoso',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'
-  }
-
-  const item4 = {
-    id: '#4',
-    nome: 'Charmander',
-    tipo: 'Fogo',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
-  }
-
-  const item5 = {
-    id: '#5',
-    nome: 'Charmeleon',
-    tipo: 'Fogo',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png'
-  }
-
-  const item6 = {
-    id: '#6',
-    nome: 'Charizard',
-    tipo: 'Fogo',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png'
-  }
-
-  const item7 = {
-    id: '#7',
-    nome: 'Squirtle',
-    tipo: 'Água',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'
-  }
-
-  const item8 = {
-    id: '#8',
-    nome: 'Wartortle',
-    tipo: 'Água',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png'
-  }
-
-  const item9 = {
-    id:'#9',
-    nome: 'Blastoise',
-    tipo: 'Água',
-    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png'
-    }
-
-  const personagens = [item1, item2, item3,item4,item5,item6,item7,item8,item9]
-
+ 
 
   return (
     <>
-      <div className='logo'><Logo/></div>
-      <div className='audio'><Audio/></div>
+      <div className='logo'><Logo /></div>
+      <div className='audio'><Audio /></div>
       <div className='lista'>
         {
-          personagens.map(function (umPersonagem) {
-            return <Card info={umPersonagem} />
+          personagens.map(function (umPersonagem, index) {
+            return <Card key={index} info={umPersonagem} />
           })
         }
       </div>
-      <div className='video'><Video/></div>
+      <div className='video'><Video /></div>
 
     </>
   )
