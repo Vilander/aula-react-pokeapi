a778488b434a4d399a4aa6f9b9211f1eb290ac42
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-07-31 21:34:56 -0300

    style: estilização do card

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,76 @@ import './App.css'
 import Card from './Card';
 
 function App() {
-  const item1 ={
-    nome: 'Rick Sanchez',
+  const item1 = {
+    id: '#1',
+    nome: 'Bulbassauro',
+    tipo: 'Planta/Venenoso',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
   }
-  const item2 ={
-    nome: 'Morty Smith',
+  const item2 = {
+    id: '#2',
+    nome: 'Ivysaur',
+    tipo: 'Planta/Venenoso',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png'
   }
-  const item3 ={
-    nome: 'Summer Smith',
+  const item3 = {
+    id: '#3',
+    nome: 'Venusaur',
+    tipo: 'Planta/Venenoso',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'
   }
-  
-  const personagens = [item1,item2,item3]
-  
+
+  const item4 = {
+    id: '#4',
+    nome: 'Charmander',
+    tipo: 'Fogo',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+  }
+
+  const item5 = {
+    id: '#5',
+    nome: 'Charmeleon',
+    tipo: 'Fogo',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png'
+  }
+
+  const item6 = {
+    id: '#6',
+    nome: 'Charizard',
+    tipo: 'Fogo',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png'
+  }
+
+  const item7 = {
+    id: '#7',
+    nome: 'Squirtle',
+    tipo: 'Água',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'
+  }
+
+  const item8 = {
+    id: '#8',
+    nome: 'Wartortle',
+    tipo: 'Água',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png'
+  }
+
+  const item9 = {
+    id:'#9',
+    nome: 'Blastoise',
+    tipo: 'Água',
+    foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png'
+    }
+
+  const personagens = [item1, item2, item3,item4,item5,item6,item7,item8,item9]
+
 
   return (
     <>
       <div className='lista'>
         {
-          personagens.map(function(umPersonagem){
-            return <Card info={umPersonagem}/>
+          personagens.map(function (umPersonagem) {
+            return <Card info={umPersonagem} />
           })
         }
       </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,9 @@ function Card(props) {
     return (
         <>
          <div className='card'>
-             <h2>{props.info.nome}</h2>
+             <h2>{props.info.id}</h2>
+             <h3>{props.info.nome}</h3>
+             <h4>{props.info.tipo}</h4>
              <img src={props.info.foto} />
          </div>
         </>
